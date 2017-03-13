c5ce7b9482e77afcd62b524faafc3749a556bce1
jonatanmendez <jonatansimpleplan@hotmail.com>
2017-03-13 15:19:26 -0600

    .

--- Apps/WorkOrdersApp/views.py
+++ Apps/WorkOrdersApp/views.py
@@ -1,5 +1,6 @@
 #encoding:utf-8
 from django.views.generic import TemplateView
+from .models import  Order
 
 class DropZoneExample(TemplateView):
     "Example of dropzone usage"
@@ -19,3 +20,10 @@ class WorkOrdersListView(TemplateView):
         context = self.get_context_data()
         print('the photo arrived!')
         return super(TemplateView, self).render_to_response(context)
+
+    def get_context_data(self, **kwargs):
+        context = super(WorkOrdersListView, self).get_context_data(**kwargs)
+        context['orders'] = Order.objects.all()
+        context['list_orders'] = 'active'
+        context['work_orders'] = 'active'
+        return context
