5cc78443c98ed34b6219a78be17f3cc5bf4168b1
jonatanmendez <jonatansimpleplan@hotmail.com>
2017-03-13 16:31:22 -0600

    Agregando modulo de edicion en las ordenes

--- Apps/WorkOrdersApp/views.py
+++ Apps/WorkOrdersApp/views.py
@@ -1,5 +1,5 @@
 #encoding:utf-8
-from django.views.generic import TemplateView, CreateView, DetailView
+from django.views.generic import TemplateView, CreateView, DetailView, UpdateView
 from django.core.urlresolvers import reverse_lazy, reverse
 from django.db import transaction
 from django.contrib import messages
@@ -67,4 +67,27 @@ class DetailOrderView(DetailView):
         context = super(DetailOrderView, self).get_context_data(**kwargs)
         context["list_orders"] = "active"
         context['work_orders'] = 'active'
+        return context
+
+
+class EditOrderView( UpdateView):
+    model = Order
+    form_class = NewOrderForm
+    template_name = 'new_orders_form.html'
+
+    def get_success_url(self):
+        return reverse('work-orders-list')
+
+
+    @transaction.atomic()
+    def form_valid(self, form):
+        object_id = form.save
+        messages.success(self.request,"Edited order")
+
+        return super(EditOrderView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(EditOrderView, self).get_context_data(**kwargs)
+        context['work_orders'] = 'active'
+        context['new_orders'] = 'active'
         return context
